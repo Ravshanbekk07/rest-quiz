d95c6b982ee8cb8a3bb51a1696af4987d52053c7
Ravshanbekk07 <ramazonof07@gmail.com>
2023-10-04 16:15:02 +0500

    model take

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Quiz,Question,Option
+from .models import Quiz,Question,Option,Take
 # Register your models here
 admin.site.register(Quiz)
 admin.site.register(Question)
 admin.site.register(Option)
+admin.site.register(Take)
